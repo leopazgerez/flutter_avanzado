715411ebf6b25cf50feabbc0d22c71c47a725691
LeoPazGerez <104148448+leopazgerez@users.noreply.github.com>
2024-07-22 19:46:19 -0300

    pages added

---
+++ real_time_chat/real_time_chat_app/lib/pages/sign_up_page.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  late final TextEditingController _emailController;
+  late final TextEditingController _nameController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _emailController = TextEditingController();
+    _nameController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _topImage(),
+              const SizedBox(
+                height: 24,
+              ),
+              _form(),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_key.currentState?.validate() ?? false) {}
+                },
+                child: const Text(
+                  'Log In',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Widget _topImage() {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.account_circle_rounded,
+          size: 35,
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          'Sign Up',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _form() {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+              decoration: const InputDecoration(
+                  labelText: 'Email',
+                  floatingLabelBehavior: FloatingLabelBehavior.never),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                // validate user email
+                bool emailValid = RegExp(
+                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                    .hasMatch(value);
+                if (!emailValid) {
+                  return 'Please enter a valid email';
+                }
+                return null;
+              }),
+          const SizedBox(
+            height: 16,
+          ),
+          TextFormField(
+              decoration: const InputDecoration(
+                  labelText: 'Nombre',
+                  floatingLabelBehavior: FloatingLabelBehavior.never),
+              validator: (value) {
+                if(value == null || value.isEmpty){
+                  return 'Por favor ingrese un nombre';
+                }
+                return null;
+              }),
+          const SizedBox(
+            height: 16,
+          ),
+          TextFormField(
+              decoration: const InputDecoration(
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                labelText: 'Password',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+
+                if (value.length < 6) {
+                  return 'Password must be at least 6 characters';
+                }
+                return null;
+              }),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
